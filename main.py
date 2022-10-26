ffd6962dedd13a5fab83abd63ec53c53ba4a1399
Sri Vidya <srividhyapatrayani18@gmail.com>
2022-10-26 14:29:22 +0530

    my first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+num = int(input("Enter a number: "))
+if (num % 2) == 0:
+   print("{0} is Even".format(num))
+else:
+   print("{0} is Odd".format(num))
+
+print("this is end of the program")
+print("this is end of the program")
+print("Im vidya")
